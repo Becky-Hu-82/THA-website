2ec9d464e19df8f10c188370788e24f0b23f4ad2
HeJialin2002 <53185918+HeJialin2002@users.noreply.github.com>
2021-09-15 00:41:26 -0500

    4

--- src/pages/PastEvents.jsx
+++ src/pages/PastEvents.jsx
@@ -1,19 +1,86 @@
 import './PastEvents.css';
 const PastEvents = () => {
   return (
-    <div class="timeline">
+    <div class="timeline" id = "pastevents">
+      <div id = "years">
+        <h1 id = "2018">
+          2018
+        </h1>
+      </div>
     <ul>
         <li>
             <div>
-                 2002 
+              <p>
+                Davis, California, 
+              </p>
+              <p>
+                July, 2018 
+              </p>
             </div>
         </li>
         <li>
             <div>
-                2005
+              <p>
+                August, 26th, 2018
+              </p>
+            </div>
+        </li>
+        <li>
+            <div>
+              <p>
+                November, 10th, 2018
+              </p>
+            </div>
+        </li>
+        <li>
+            <div>
+              <p>
+                November, 18th, 2018
+              </p>
             </div>
         </li>
     </ul>
+    <div id = "years">
+      <h1 id = "2019">
+          2019
+      </h1>
+    </div>
+    <div class="timeline" id = "pastevents">
+    <ul>
+        <li>
+            <div>
+              <p>
+                December 29th, 2018 
+              </p>
+              <p>
+                to
+              </p>
+              <p>
+                January 9th, 2019
+              </p>
+            </div>
+        </li>
+        <li>
+            <div>
+              <p>
+                August, 26th, 2018
+              </p>
+            </div>
+        </li>
+        <li>
+            <div>
+              <p>
+                April 9th, 2019
+              </p>
+            </div>
+        </li>
+        <div>
+          <h1>
+            and many more to come...
+          </h1>
+        </div>
+    </ul>
+  </div>
   </div>
   )
 };
