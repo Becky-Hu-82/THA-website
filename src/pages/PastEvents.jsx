a2375d782e4d5a2758898a746134c782bd7907ce
HeJialin2002 <53185918+HeJialin2002@users.noreply.github.com>
2021-09-14 21:24:19 -0500

    2

--- src/pages/PastEvents.jsx
+++ src/pages/PastEvents.jsx
@@ -1,18 +1,9 @@
 //import './PastEvents.css';
 const PastEvents = () => {
   return (
-    <div id='home'>
-    <p className='aboutText'>
-        The TreeHouse Academia (THA) was founded in the summer of 2018 by a
-        group of philanthropic driven young professionals. The THA team share a
-        common belief that young learners around the globe should have equal
-        access to education without the barriers of wealth and social classes.
-        The THA thrives to nourish the education dreams of underprivileged
-        teenagers by constructing a platform for Chinese international students
-        around the globe to donate funds, purchase educational accessories, and
-        gain social hands-on experience by visiting schools and students in
-        rural China to ensure equal education for all.
-      </p>
+    < div class = "datagrid-mac-ower">
+      < ul class = "time-vertical" id = "ower-result">
+      </ul>
     </div>
     )
 };
