1c79e8653805cdacbb72d72c4a528f3d06756504
HeJialin2002 <53185918+HeJialin2002@users.noreply.github.com>
2021-09-19 21:19:47 -0500

    1

--- src/pages/PastEvents.jsx
+++ src/pages/PastEvents.jsx
@@ -2,85 +2,87 @@ import './PastEvents.css';
 const PastEvents = () => {
   return (
     <div class="timeline" id = "pastevents">
-      <div id = "years">
-        <h1 id = "2018">
-          2018
-        </h1>
-      </div>
-    <ul>
-        <li>
-            <div>
-              <p>
-                Davis, California, 
-              </p>
-              <p>
-                July, 2018 
-              </p>
-            </div>
-        </li>
-        <li>
-            <div>
-              <p>
-                August, 26th, 2018
-              </p>
-            </div>
-        </li>
-        <li>
-            <div>
-              <p>
-                November, 10th, 2018
-              </p>
-            </div>
-        </li>
-        <li>
-            <div>
-              <p>
-                November, 18th, 2018
-              </p>
-            </div>
-        </li>
-    </ul>
-    <div id = "years">
-      <h1 id = "2019">
-          2019
-      </h1>
-    </div>
-    <div class="timeline" id = "pastevents">
-    <ul>
-        <li>
-            <div>
-              <p>
-                December 29th, 2018 
-              </p>
-              <p>
-                to
-              </p>
-              <p>
-                January 9th, 2019
-              </p>
-            </div>
-        </li>
-        <li>
-            <div>
-              <p>
-                August, 26th, 2018
-              </p>
-            </div>
-        </li>
-        <li>
-            <div>
-              <p>
-                April 9th, 2019
-              </p>
-            </div>
-        </li>
-        <div>
-          <h1>
-            and many more to come...
+      <div id = "part1">
+        <div id = "years">
+          <h1 id = "2018">
+           2018
           </h1>
         </div>
-    </ul>
-  </div>
+      <ul>
+          <li>
+              <div>
+                <p>
+                  Davis, California, 
+                </p>
+                <p>
+                  July, 2018 
+                </p>
+              </div>
+          </li>
+          <li>
+              <div>
+                <p>
+                  August, 26th, 2018
+                </p>
+              </div>
+          </li>
+          <li>
+              <div>
+                <p>
+                  November, 10th, 2018
+                </p>
+              </div>
+          </li>
+          <li>
+              <div>
+                <p>
+                  November, 18th, 2018
+                </p>
+              </div>
+          </li>
+      </ul>
+    </div>
+    <div id = "part2">
+      <div id = "years">
+        <h1 id = "2019">
+            2019
+        </h1>
+      </div>
+      <ul>
+          <li>
+              <div>
+                <p>
+                  December 29th, 2018 
+                </p>
+                <p>
+                  to
+                </p>
+                <p>
+                  January 9th, 2019
+                </p>
+              </div>
+          </li>
+          <li>
+              <div>
+                <p>
+                  August, 26th, 2018
+                </p>
+              </div>
+          </li>
+          <li>
+              <div>
+                <p>
+                  April 9th, 2019
+                </p>
+              </div>
+          </li>
+          <div>
+            <h1>
+              and many more to come...
+            </h1>
+          </div>
+      </ul>
+    </div>
   </div>
   )
 };
