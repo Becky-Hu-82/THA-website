97f268c1de521924b0281a50d39dbdeffd55dc44
Becky Hu <bh456@cornell.edu>
2021-11-12 09:54:04 -0500

    fixed home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import GeneralBackground from '../components/GeneralBackground/GeneralBackground';
 import './Home.css';
-import img from './Img/WePage.jpg'
+import img from './Img/WePage.jpg';
 const Home = () => {
-    //const sections = [<p>we</p>];
-    //return <GeneralBackground sections={sections} />;
-    return (
-        <div id='home'>
-            <img src={img} id='image' />
-        </div>
-        )
-  };
-  
-  
-  
+  //const sections = [<p>we</p>];
+  //return <GeneralBackground sections={sections} />;
+  return (
+    <div id='home'>
+      <img src={img} id='homeImage' />
+    </div>
+  );
+};
+
 export default Home;
